843771d1f72d39af114cf539f2f9f87f0319dc79
Unknown <liweihu12@gmail.com>
2019-02-11 09:40:47 +0100

    Added tests for tweet and trend

--- src/test/java/com/kwetter/domain/TrendTest.java
+++ src/test/java/com/kwetter/domain/TrendTest.java
@@ -3,19 +3,56 @@ package com.kwetter.domain;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import static org.junit.Assert.*;
 
 public class TrendTest {
+    private UUID testId;
+    private Trend testTrend;
+    private Trend testEmptyTrend;
+    private List<Tweet> testTweets;
+    private String testName;
+
     @Before
     public void setup(){
-
+        this.testId = UUID.randomUUID();
+        this.testTweets = new ArrayList<>();
+        this.testName = "#Test";
+        this.testTrend = new Trend(testId, testName, testTweets);
+        this.testEmptyTrend = new Trend();
     }
 
     @Test
+    public void getIdTest(){
+        assertEquals("The ID was not right.", this.testId, testTrend.getId());
+    }
+
+    @Test
+    public void getEmptyIdTest(){
+        assertNull("The ID was not right.", testEmptyTrend.getId());
+    }
+
+
+    @Test
     public void getNameTest() {
+        assertEquals("The name was not right.", testName, this.testTrend.getName());
+    }
+
+    @Test
+    public void getEmptyNameTest() {
+        assertNull("The name was not empty.", this.testEmptyTrend.getName());
     }
 
     @Test
     public void getTweetsTest() {
+        assertNotNull("The tweet list was null.", testTrend.getTweets());
+    }
+
+    @Test
+    public void getEmptyTweetsTest() {
+        assertNull("The tweet list was not null.", testEmptyTrend.getTweets());
     }
 }
--- src/test/java/com/kwetter/domain/TweetTest.java
+++ src/test/java/com/kwetter/domain/TweetTest.java
@@ -3,41 +3,97 @@ package com.kwetter.domain;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.UUID;
+
 import static org.junit.Assert.*;
 
 public class TweetTest {
+    private Tweet testTweet;
+    private Tweet testTweetEmpty;
+    private User testUser;
+    private UUID testId;
+    private String testMessage;
+    private Date testDate;
+
     @Before
     public void setup() {
-
+        testId = UUID.randomUUID();
+        testUser = new User(UUID.randomUUID(), "Test", "Test", "Test bio", "Reee", "Skkrt", new ArrayList<User>(), new ArrayList<User>(), new ArrayList<Role>(), new ArrayList<Tweet>(), new ArrayList<Tweet>(), new ArrayList<Tweet>());
+        testMessage = "This is a test lol";
+        testDate = new Date();
+        testTweetEmpty = new Tweet();
+        testTweet = new Tweet(testId, testUser, testMessage, testDate, new ArrayList<User>(), new ArrayList<User>(), new ArrayList<Trend>());
     }
 
     @Test
     public void getIdTest() {
+        assertEquals("The id was not right.", this.testId, testTweet.getId());
+    }
 
+    @Test
+    public void getEmptyIdTest() {
+        assertNull("The id was not empty.", testTweetEmpty.getId());
     }
 
     @Test
     public void getMessageTest() {
-
+        assertEquals("The message was not right", this.testMessage, testTweet.getMessage());
     }
 
     @Test
-    public void getDateCreatedTest() {
+    public void getEmptyMessageTest() {
+        assertNull("The message was not right", testTweetEmpty.getMessage());
+    }
 
+    @Test
+    public void getAuthorTest() {
+        assertEquals("The author was not right.", this.testUser, testTweet.getAuthor());
+    }
+
+    @Test
+    public void getEmptyAuthorTest() {
+        assertNull("The author was not empty.", testTweetEmpty.getAuthor());
     }
 
     @Test
     public void getMentionsTest() {
+        assertNotNull("The mentions was null.", testTweet.getMentions());
+    }
 
+    @Test
+    public void getEmptyMentionsTest() {
+        assertNull("The mentions was not null.", testTweetEmpty.getMentions());
     }
 
     @Test
     public void getHeartedByTest() {
+        assertNotNull("The hearted by list was null.", testTweet.getHeartedBy());
+    }
 
+    @Test
+    public void getEmptyHeartedByTest() {
+        assertNull("The hearted by list was not null.", testTweetEmpty.getHeartedBy());
+    }
+
+    @Test
+    public void getDateCreatedTest() {
+        assertEquals("The Date was not right.", this.testDate, testTweet.getDateCreated());
+    }
+
+    @Test
+    public void getEmptyDateCreatedTest() {
+        assertNull("The Date was not right.", testTweetEmpty.getDateCreated());
     }
 
     @Test
     public void getTrendsTest() {
+        assertNotNull("The trends list was null.", testTweet.getTrends());
+    }
 
+    @Test
+    public void getEmptyTrendsTest() {
+        assertNull("The trends list was not null.", testTweetEmpty.getTrends());
     }
 }

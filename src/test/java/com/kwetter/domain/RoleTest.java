1a338958629c89ccb9306f1d68f60b31165a8403
Unknown <liweihu12@gmail.com>
2019-02-11 11:13:29 +0100

    100% line coverage with 10 test users

--- src/test/java/com/kwetter/domain/RoleTest.java
+++ src/test/java/com/kwetter/domain/RoleTest.java
@@ -11,37 +11,37 @@ import java.util.UUID;
 import static org.junit.Assert.*;
 
 public class RoleTest {
-    private User testUser;
-    private User testAdministrator;
-    private User testModerator;
+    private User testUser1;
+    private User testAdministrator1;
+    private User testModerator1;
 
     @Before
     public void setup() {
-        testUser = new User(UUID.randomUUID(), "Test", "Test", "Test bio", "", "", null, null, new ArrayList<Role>() {{
+        testUser1 = new User(UUID.randomUUID(), "Test", "Test", "Test bio", "", "", null, null, new ArrayList<Role>() {{
             add(Role.Standard);
         }}, null, null, null);
 
-        testAdministrator = new User(UUID.randomUUID(), "Test", "Test", "Test bio", "", "", null, null, new ArrayList<Role>() {{
+        testAdministrator1 = new User(UUID.randomUUID(), "Test", "Test", "Test bio", "", "", null, null, new ArrayList<Role>() {{
             add(Role.Administrator);
         }}, null, null, null);
 
-        testModerator = new User(UUID.randomUUID(), "Test", "Test", "Test bio", "", "", null, null, new ArrayList<Role>() {{
+        testModerator1 = new User(UUID.randomUUID(), "Test", "Test", "Test bio", "", "", null, null, new ArrayList<Role>() {{
             add(Role.Moderator);
         }}, null, null, null);
     }
 
     @Test
     public void checkStandardRole() {
-        assertEquals(Role.Standard, testUser.getRoles().get(0));
+        assertEquals(Role.Standard, testUser1.getRoles().get(0));
     }
 
     @Test
     public void checkAdministratorRole() {
-        assertEquals(Role.Administrator, testAdministrator.getRoles().get(0));
+        assertEquals(Role.Administrator, testAdministrator1.getRoles().get(0));
     }
 
     @Test
     public void checkModeratorRole() {
-        assertEquals(Role.Moderator, testModerator.getRoles().get(0));
+        assertEquals(Role.Moderator, testModerator1.getRoles().get(0));
     }
 }

2d4683b4f4036dfaf001996f27e06b1b76d5587a
Unknown <liweihu12@gmail.com>
2019-02-15 19:36:23 +0100

    Changes to Persistence xml, db not creating schemas on startup

---
+++ src/main/java/com/kwetter/KwetterApplication.java
@@ -0,0 +1,11 @@
+package com.kwetter;
+
+
+import javax.ws.rs.core.Application;
+
+public class KwetterApplication extends Application {
+
+    public static void main(String[] args){
+        
+    }
+}

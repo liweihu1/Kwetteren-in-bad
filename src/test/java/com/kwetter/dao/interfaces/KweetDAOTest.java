a3bfb08d527501eb5dc76935d1f88e8a1a83a96f
Unknown <liweihu12@gmail.com>
2019-02-26 20:41:23 +0100

    Add Tests for DAO

---
+++ src/test/java/com/kwetter/dao/interfaces/KweetDAOTest.java
@@ -0,0 +1,52 @@
+package com.kwetter.dao.interfaces;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class KweetDAOTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void getAllKweets() {
+    }
+
+    @Test
+    public void getKweetLikesForUserId() {
+    }
+
+    @Test
+    public void getAllKweetsByUserId() {
+    }
+
+    @Test
+    public void getKweetForUserIdWithFollowers() {
+    }
+
+    @Test
+    public void getLatestKweetsForUserId() {
+    }
+
+    @Test
+    public void getKweetThatContainsSearch() {
+    }
+
+    @Test
+    public void clearData() {
+    }
+}
---
+++ src/test/java/com/kwetter/dao/interfaces/UserDAOTest.java
@@ -0,0 +1,52 @@
+package com.kwetter.dao.interfaces;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserDAOTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void checkUsernameAvailable() {
+    }
+
+    @Test
+    public void getAllUsers() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void findByUsername() {
+    }
+
+    @Test
+    public void getFollowing() {
+    }
+
+    @Test
+    public void getFollowers() {
+    }
+
+    @Test
+    public void getUserMentions() {
+    }
+
+    @Test
+    public void clearData() {
+    }
+}

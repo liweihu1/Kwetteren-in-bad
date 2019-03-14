42362ca9699120083237b55966db0c9774d2567a
Unknown <liweihu12@gmail.com>
2019-03-14 14:26:33 +0100

    Add Kweet Removal dashboard

--- src/main/java/com/kwetter/controllers/KweetRemovalDashboardController.java
+++ src/main/java/com/kwetter/controllers/KweetRemovalDashboardController.java
@@ -1,14 +1,43 @@
 package com.kwetter.controllers;
 
+import com.kwetter.domain.Kweet;
+import com.kwetter.domain.User;
+import com.kwetter.service.KweetService;
+import com.kwetter.service.UserService;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
+import javax.inject.Inject;
+import java.util.List;
 import java.util.UUID;
 
-@SessionScoped
+@RequestScoped
 @ManagedBean(name = "KweetRemovalDashboardController")
 public class KweetRemovalDashboardController {
 
-    @ManagedProperty(value = "#{param.id}")
-    private UUID selectedUserID;
+    @ManagedProperty("#{param['id']}")
+    private String userId;
+
+    @Inject
+    private KweetService kweetService;
+
+    @Inject
+    private UserService userService;
+
+    public List<Kweet> getKweets(){
+        return kweetService.getKweetsForUserId(UUID.fromString(userId));
+    }
+
+    public User getUser(){
+        return userService.getUserById(UUID.fromString(userId));
+    }
+
+    public String getUserId(){
+        return this.userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
